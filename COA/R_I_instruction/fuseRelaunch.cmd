f298fee5812a952ab0611575f5f670beadd9abd7
messengerW <messenger232624@gmail.com>
2019-06-13 15:02:02 +0800

    via

---
+++ COA/R_I_instruction/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/ISE/workspace/Computer-Experiment-on-the-principle-of-computer-composition-master/R_I_instruction/test_isim_beh.exe" -prj "C:/ISE/workspace/Computer-Experiment-on-the-principle-of-computer-composition-master/R_I_instruction/test_beh.prj" "work.test" "work.glbl" 
